6f25db48c4374a65aaa268018eb197333857efe8
Tom Smith <thomas.smith1@imperial.ac.uk>
2021-06-21 11:56:26 +0100

    first look at adding UK spatial units

---
+++ src/temporary-uk-pipeline.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Run script to download and then average climate variable across spatial units
+# Tom Smith 2021
+
+echo "Run cds download script over a particular date range"
+
+python3 src/cds-era5-args.py -y 2021 -m 05 -c temperature humidity uv precipitation
+
+echo "Average across single days"
+
+cdo daymean data/cds-temp.grib data/cds-temp-dailymean.grib
+cdo daymean data/cds-humid.grib data/cds-humid-dailymean.grib
+cdo daymean data/cds-uv.grib data/cds-uv-dailymean.grib
+cdo daymean data/cds-precip.grib data/cds-precip-dailymean.grib
+
+echo "Average daily climate data across spatial regions"
+
+Rscript src/clean-cds-era5-uk.R -y 2021 -m 05
+
+echo "... Finished averaging across regions! Files written to output directory!"
