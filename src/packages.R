fe3af12253973e1078d1ab33a75fa4da0f383442
Francis Windram <francis.windram17@imperial.ac.uk>
2025-02-27 15:28:17 +0000

    Add cli and rlang to imports for better messaging and interactive handling (these are already dplyr deps)

--- src/packages.R
+++ src/packages.R
@@ -2,7 +2,7 @@ silent.require <- function(x) suppressMessages(require(package=x, character.only
 
 # Load packages that are already installed
 packages <- c("optparse", "terra", "sf", "exactextractr", "dplyr", "tidyr", "parallel",
-              "ggplot2", "viridis", "lubridate"
+              "ggplot2", "viridis", "lubridate", "cli", "rlang"
 )
 
 ready <- sapply(packages, silent.require)
