9b01bd773c4ab676ea132b4dc8191b0cd1181437
Francis Windram <francis.windram17@imperial.ac.uk>
2025-02-28 17:33:16 +0000

    Refactor data downloader and add new flag to optionally output to a subdirectory

--- src/cds-era5-args.py
+++ src/cds-era5-args.py
@@ -1,33 +1,29 @@
 import cdsapi
 import argparse
+import sys
+
+from multiurl import download
 
 text = 'Download cds data with command date selections.'
 
 parser = argparse.ArgumentParser(description=text)
 parser.add_argument("-V", "--version", help="show program version", action="store_true")
-parser.add_argument("-y", "--years", nargs='+', help="set download years (required), e.g. -y 2020 2021")
-parser.add_argument("-m", "--months", nargs='+', help="set download months (required), e.g. -m 01 02 03")
+parser.add_argument("-y", "--years", nargs='+', help="set download years (required), e.g. -y 2020 2021", required=True)
+parser.add_argument("-m", "--months", nargs='+', help="set download months (required), e.g. -m 01 02 03", required=True)
 parser.add_argument("-d", "--days", nargs='+', help="set download days, e.g. -d 10 11 12; if not set, will default to all days (01 - 31)")
-parser.add_argument("-c", "--climvars", nargs='+', help="set climate variable(s) to download, currently supporting: 'temperature', 'spec_humid', 'rel_humid', 'uv', 'precipitation', e.g. -c temperature rel_humid")
+parser.add_argument("-c", "--climvars", nargs='+', help="set climate variable(s) to download, currently supporting: 'temperature', 'spec_humid', 'rel_humid', 'uv', 'precipitation', e.g. -c temperature rel_humid", required=True)
+parser.add_argument("-f", "--folder", help="output to folder named according to parameters", action="store_true")
 
 args = parser.parse_args()
 
-
-
 if args.version:
-    print("This is version 0.3 of the CDS download script")
-if args.climvars:
-    print("Downloading climate variable(s) %s" % args.climvars)
-if args.years:
-    print("Download for year(s) %s" % args.years)
-if args.months:
-    print("Download for month(s) %s" % args.months)
+    print("This is version 0.4 of the CDS download script")
 
+# Construct days list if not provided
 if args.days:
-	dldays = args.days
-	print("Download for day(s) %s" % dldays)
+    dldays = args.days
 else:
-	dldays = ['01', '02', '03',
+    dldays = ['01', '02', '03',
             '04', '05', '06',
             '07', '08', '09',
             '10', '11', '12',
@@ -38,163 +34,78 @@ else:
             '25', '26', '27',
             '28', '29', '30',
             '31']
-	print("Download for day(s) %s" % dldays)
 
+print("Downloading climate variable(s) %s" % args.climvars)
+print("Download for year(s) %s" % args.years)
+print("Download for month(s) %s" % args.months)
+print("Download for day(s) %s" % dldays)
+
+outfolder = "data"
+
+# Construct output folder if required
+if args.folder:
+    outfolder = "data/{}_{}".format("-".join(args.years), "-".join(args.months))
+    print("Output folder data/{}".format(outfolder))
+
+# Force line break for nice formatting
+print()
+
+# Data store for supported rasters. When you add new ones from era5 you may just need to add a line here
+rasterlookup = {
+    "temperature": {"name": "reanalysis-era5-pressure-levels", "variable": "temperature", "filename": "cds-temp.grib"},
+    "spec_humid": {"name": "reanalysis-era5-pressure-levels", "variable": "specific_humidity", "filename": "cds-spechumid.grib"},
+    "rel_humid": {"name": "reanalysis-era5-pressure-levels", "variable": "relative_humidity", "filename": "cds-relhumid.grib"},
+    "uv": {"name": "reanalysis-era5-single-levels", "variable": "downward_uv_radiation_at_the_surface", "filename": "cds-uv.grib"},
+    "precipitation": {"name": "reanalysis-era5-single-levels", "variable": "total_precipitation", "filename": "cds-precip.grib"}
+}
+
+notified = False
+
+if not (args.years and args.months and dldays):
+    # Should never trigger as these required arguments are now enforced by argparse
+    print("Error: You didn't specify one of the required arguments - see --help for more details.")
+    sys.exit(1)
 
 # Run download code depending on which climate variable is selected
-if 'temperature' in args.climvars:
-	# need to have the specified arguments for the script to run
-	if args.years and args.months and dldays:
-		# do all the CDS stuff
-		c = cdsapi.Client()
-		
-		c.retrieve(
-		'reanalysis-era5-pressure-levels',
-		{
-			'product_type': 'reanalysis',
-			'variable': 'temperature',
-			'pressure_level': '1000',
-			'year': args.years,
-			'month': args.months,
-			'day': dldays,
-			'time': [
-				'00:00', '01:00', '02:00',
-				'03:00', '04:00', '05:00',
-				'06:00', '07:00', '08:00',
-				'09:00', '10:00', '11:00',
-				'12:00', '13:00', '14:00',
-				'15:00', '16:00', '17:00',
-				'18:00', '19:00', '20:00',
-				'21:00', '22:00', '23:00',
-			],
-			'format': 'grib',
-		},
-		'data/cds-temp.grib')
-	else:
-		print("Error: You didn't specify one of the required arguments - see --help for more details.")
+for climvar in args.climvars:
+    # Try to get the data for the provided climatic variable
+    try:
+        climvardata = rasterlookup[climvar]
+    except KeyError:
+        # If not present, notify and skip
+        print("\nERROR: '{}' is not a valid variable!".format(climvar))
+        if not notified:
+            # Only show once
+            print("\nValid options are:\n  - {}".format("\n  - ".join(list(rasterlookup.keys()))))
+            notified = True
+        continue
 
+    print("Retrieving {}...".format(climvar))
 
-if 'spec_humid' in args.climvars:
-	# need to have the specified arguments for the script to run
-	if args.years and args.months and dldays:
-		# do all the CDS stuff
-		c = cdsapi.Client()
-		
-		c.retrieve(
-		'reanalysis-era5-pressure-levels',
-		{
-			'product_type': 'reanalysis',
-			'variable': 'specific_humidity',
-			'pressure_level': '1000',
-			'year': args.years,
-			'month': args.months,
-			'day': dldays,
-			'time': [
-				'00:00', '01:00', '02:00',
-				'03:00', '04:00', '05:00',
-				'06:00', '07:00', '08:00',
-				'09:00', '10:00', '11:00',
-				'12:00', '13:00', '14:00',
-				'15:00', '16:00', '17:00',
-				'18:00', '19:00', '20:00',
-				'21:00', '22:00', '23:00',
-			],
-			'format': 'grib',
-		},
-		'data/cds-spechumid.grib')
-	else:
-		print("Error: You didn't specify one of the required arguments - see --help for more details.")
+    # do all the CDS stuff
+    c = cdsapi.Client()
 
+    c.retrieve(
+        climvardata["name"],
+        {
+            'product_type': 'reanalysis',
+            'variable': climvardata["variable"],
+            'pressure_level': '1000',
+            'year': args.years,
+            'month': args.months,
+            'day': dldays,
+            'time': [
+                '00:00', '01:00', '02:00',
+                '03:00', '04:00', '05:00',
+                '06:00', '07:00', '08:00',
+                '09:00', '10:00', '11:00',
+                '12:00', '13:00', '14:00',
+                '15:00', '16:00', '17:00',
+                '18:00', '19:00', '20:00',
+                '21:00', '22:00', '23:00',
+            ],
+            'format': 'grib',
+        },
+        '{}/{}'.format(outfolder, climvardata["filename"]))
 
-if 'rel_humid' in args.climvars:
-	# need to have the specified arguments for the script to run
-	if args.years and args.months and dldays:
-		# do all the CDS stuff
-		c = cdsapi.Client()
-		
-		c.retrieve(
-		'reanalysis-era5-pressure-levels',
-		{
-			'product_type': 'reanalysis',
-			'variable': 'relative_humidity',
-			'pressure_level': '1000',
-			'year': args.years,
-			'month': args.months,
-			'day': dldays,
-			'time': [
-				'00:00', '01:00', '02:00',
-				'03:00', '04:00', '05:00',
-				'06:00', '07:00', '08:00',
-				'09:00', '10:00', '11:00',
-				'12:00', '13:00', '14:00',
-				'15:00', '16:00', '17:00',
-				'18:00', '19:00', '20:00',
-				'21:00', '22:00', '23:00',
-			],
-			'format': 'grib',
-		},
-		'data/cds-relhumid.grib')
-	else:
-		print("Error: You didn't specify one of the required arguments - see --help for more details.")
-
-
-if 'uv' in args.climvars:
-	# need to have the specified arguments for the script to run
-	if args.years and args.months and dldays:
-		# do all the CDS stuff
-		c = cdsapi.Client()
-		
-		c.retrieve(
-		'reanalysis-era5-single-levels',
-		{
-			'product_type': 'reanalysis',
-			'variable': 'downward_uv_radiation_at_the_surface',
-			'year': args.years,
-			'month': args.months,
-			'day': dldays,
-			'time': [
-				'00:00', '01:00', '02:00',
-				'03:00', '04:00', '05:00',
-				'06:00', '07:00', '08:00',
-				'09:00', '10:00', '11:00',
-				'12:00', '13:00', '14:00',
-				'15:00', '16:00', '17:00',
-				'18:00', '19:00', '20:00',
-				'21:00', '22:00', '23:00',
-			],
-			'format': 'grib',
-		},
-		'data/cds-uv.grib')
-	else:
-		print("Error: You didn't specify one of the required arguments - see --help for more details.")
-
-
-if 'precipitation' in args.climvars:
-	# need to have the specified arguments for the script to run
-	if args.years and args.months and dldays:
-		# do all the CDS stuff
-		c = cdsapi.Client()
-		
-		c.retrieve(
-		'reanalysis-era5-single-levels',
-		{
-			'product_type': 'reanalysis',
-			'variable': 'total_precipitation',
-			'year': args.years,
-			'month': args.months,
-			'day': dldays,
-			'time': [
-				'00:00', '01:00', '02:00',
-				'03:00', '04:00', '05:00',
-				'06:00', '07:00', '08:00',
-				'09:00', '10:00', '11:00',
-				'12:00', '13:00', '14:00',
-				'15:00', '16:00', '17:00',
-				'18:00', '19:00', '20:00',
-				'21:00', '22:00', '23:00',
-			],
-			'format': 'grib',
-		},
-		'data/cds-precip.grib')
-	else:
-		print("Error: You didn't specify one of the required arguments - see --help for more details.")
+print("\nDownload complete!")
